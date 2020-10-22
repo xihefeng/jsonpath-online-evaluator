2fdc443f746f365fdcc0733f8062e7b614749e85
xihefeng <xihefeng@users.noreply.github.com>
2020-10-22 20:03:17 +0800

    Update webpack.common.js

--- webpack.common.js
+++ webpack.common.js
@@ -2,6 +2,7 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const StylelintPlugin = require('stylelint-webpack-plugin');
+const CopyPlugin = require('copy-webpack-plugin');
 
 module.exports = {
   entry: './app/scripts/main.js',
